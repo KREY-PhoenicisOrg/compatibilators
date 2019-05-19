3527ce6562467559432b7b26c5812eed21c895f2
Jacob Hrbek <werifgx@gmail.com>
2019-05-20 01:38:42 +0200

    GAME_OF_TRUE_VIRGINS

---
+++ Applications/Games/MagicTheGatheringArena/Online/script.js
@@ -0,0 +1,28 @@
+include("engines.wine.quick_script.online_installer_script");
+include("engines.wine.verbs.dxvk");
+include("engines.wine.plugins.usetakefocus")
+
+// Install Magic: The Gathering Arena
+/// RELAVANT: https://github.com/PhoenicisOrg/scripts/issues/934
+
+ var installerImplementation = {
+    run: function () {
+        new OnlineInstallerScript()
+            .name("Magic: The Gathering Arena")
+            .applicationHomepage("https://magic.wizards.com/")
+            .url("https://mtgarena.downloads.wizards.com/Live/Windows32/MTGAInstaller.exe")
+            .author("KREYREN")
+            .category("Games")
+            .wineDistribution("staging")
+            .wineVersion(LATEST_STAGING_VERSION)
+            .preInstall(function (wine/*, wizard*/) {
+              wine.DXVK();
+              wine.UseTakeFocus("N");
+            })
+            .executable("MTGAInstaller.exe")
+            .go();
+    }
+};
+
+ /* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
